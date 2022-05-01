746e8b8b872195eced38ca11cbf484f67e5a8833
Visakh <56556101+rvishakhs@users.noreply.github.com>
2022-05-02 00:57:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,51 @@
 const todoinput = document.getElementById("todo-input");
 const todobtn = document.getElementById("todo-btn");
-const todolist = document.getElementById("todo-list")
+const todolist = document.getElementById("todo-list");
 let mylists = [];
 
 
-todobtn.addEventListener("click", function(){
+todolist.addEventListener("click", function(e){
+    let item = e.target;
+    if(item.classList[0] === "trash-btn"){
+        const element = item.parentElement;
+        element.setAttribute('id', 'fall')
+        element.addEventListener('transitionend', function(){
+            element.remove();   
+        })  
+    }
+
+    if(item.classList[0] === "complete-btn"){
+        const element = item.parentElement;
+        element.setAttribute('id', 'completed')
+    }
+})
+
+todobtn.addEventListener("click", function(){ 
     event.preventDefault();
     listDiv()
     todoinput.value = ""
 })
 
-
-
 function listDiv(){
     // Todo Div
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
+    todoDiv.setAttribute('id', 'todo');
+
 
     //Create LI
     
     const newTodo = document.createElement("li");
-    newTodo.classList.add("todo-item")
+    newTodo.classList.add("todo-item");
+    newTodo.setAttribute('id', 'todo-item');
     newTodo.innerText = todoinput.value;
-    todoDiv.appendChild(newTodo)
+    todoDiv.appendChild(newTodo);
     
     // create check mark Button
 
     const completedButton = document.createElement("button");
     completedButton.classList.add("complete-btn");
+    newTodo.setAttribute('id', 'todo-item');
     completedButton.innerHTML = '<i class="fa-solid fa-check"></i>';
     todoDiv.appendChild(completedButton)
 
@@ -41,6 +59,4 @@ function listDiv(){
     todolist.append(todoDiv)
 }
 
-function deletecheck(){
 
-}
