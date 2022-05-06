9314f0a1d38c62866d2487126c865be5d5902472
Visakh <56556101+rvishakhs@users.noreply.github.com>
2022-05-06 22:38:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,53 +4,15 @@ const todolist = document.getElementById("todo-list");
 const filtertodo = document.getElementById("filter-todo")
 let mylists = [];
 
-document.addEventListener("DOMcontentLoaded", function(){
-
-    let listFromLocal = JSON.parse(localStorage.getItem("mylists"))
-
-    if (listFromLocal){
-    mylists = listFromLocal
-    mylists.forEach(function(todo) {
-        // Todo Div
-       const todoDiv = document.createElement("div");
-       todoDiv.classList.add("todo");
-       todoDiv.setAttribute('id', 'todo');
-   
-   
-       //Create LI
-       
-       const newTodo = document.createElement("li");
-       newTodo.classList.add("todo-item");
-       newTodo.setAttribute('id', 'todo-item');
-       newTodo.innerText = "My name is ";
-       todoDiv.appendChild(newTodo);
-       
-       // create check mark Button
-   
-       const completedButton = document.createElement("button");
-       completedButton.classList.add("complete-btn");
-       newTodo.setAttribute('id', 'todo-item');
-       completedButton.innerHTML = '<i class="fa-solid fa-check"></i>';
-       todoDiv.appendChild(completedButton)
-   
-       // Create trash button
-   
-       const trashButton = document.createElement("button");
-       trashButton.classList.add("trash-btn");
-       trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-       todoDiv.appendChild(trashButton)     
-       })
-    }
-
-}) 
-
+document.addEventListener("DOMContentLoaded", getTodos);
 todolist.addEventListener("click", function(e){
     let item = e.target;
     if(item.classList[0] === "trash-btn"){
         const element = item.parentElement;
         element.setAttribute('id', 'fall')
+        removeFromLocalStorage(element)  
         element.addEventListener('transitionend', function(){
-            element.remove();   
+             element.remove(); 
         })  
     }
 
@@ -58,6 +20,7 @@ todolist.addEventListener("click", function(e){
         const element = item.parentElement;
         element.setAttribute('id', 'completed')
         element.classList.add("completed")
+
     }
 })
 
@@ -89,7 +52,6 @@ todobtn.addEventListener("click", function(){
     event.preventDefault();
     listDiv()
     todoinput.value = ""
-    localStorage.setItem("localTodos", JSON.stringify(mylists));
 })
 
 
@@ -106,7 +68,9 @@ function listDiv(){
     newTodo.classList.add("todo-item");
     newTodo.setAttribute('id', 'todo-item');
     newTodo.innerText = todoinput.value;
-    mylists.push(todoinput.value);
+    // we need to call the  save local storage function because we need to pass the present input field value 
+    //  to the existing array
+    saveLocalTodos(todoinput.value);
     todoDiv.appendChild(newTodo);
     
     // create check mark Button
@@ -126,3 +90,91 @@ function listDiv(){
 
     todolist.append(todoDiv)
 }
+
+
+function saveLocalTodos(todo){    
+    let todos;
+    //checking if we have anything in the local storage if have something we need to get back to todo variable//
+    if(localStorage.getItem("todos") === null ){
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
+    //collecting back the local storage and add the values currently we entered in the input feild and
+    //save this back to local storage 
+    todos.push(todo)
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+}
+/* This function is using for get back the local storage items and show them in the DOM */
+
+function getTodos(){
+/* first we need to check if anything has in local storage if exist get it back */
+    let todos;
+    if(localStorage.getItem("todos") === null ){
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+/* and execute the code we are used to show items in DOM */
+    todos.forEach(function(todo){
+    // Todo Div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    todoDiv.setAttribute('id', 'todo');
+
+
+    //Create LI
+    
+    const newTodo = document.createElement("li");
+    newTodo.classList.add("todo-item");
+    newTodo.setAttribute('id', 'todo-item');
+    // NOW WE NEED TO CHANGE THE INNER TEXT BECAUSE WE ARE USING THE VALUE GET BACK FROM LOCAL sTORAGE
+    newTodo.innerText = todo;
+    todoDiv.appendChild(newTodo);
+    
+    // create check mark Button
+
+    const completedButton = document.createElement("button");
+    completedButton.classList.add("complete-btn");
+    newTodo.setAttribute('id', 'todo-item');
+    completedButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+    todoDiv.appendChild(completedButton)
+
+    // Create trash button
+
+    const trashButton = document.createElement("button");
+    trashButton.classList.add("trash-btn");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    todoDiv.appendChild(trashButton)
+    
+    todolist.append(todoDiv)
+    })
+
+}
+
+/* // There is a problem exist ie; after deleting somethings from DOM if we refresh the page the deleted items 
+still come to the DOM because we Didn't delete them from local storage  */
+
+    function removeFromLocalStorage(todo){
+/* Check any thing exist in Local storage */
+    let todos;
+    if(localStorage.getItem("todos") === null ){
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }    
+/*     Then we need to grab the innertext of deleted item to get the index number */
+    const todoIndex = todo.children[0].innerText
+/*     now we save the inner text to a variable 
+    todo comes from the delete button function 
+    childer[0] means getting the li from code ( li has the text we entered)
+    .innertext for grabbing the text */
+    todos.splice(todos.indexOf(todoIndex), 1);
+    /* the line execute the delete function from todo array and we are finding the indexof todoIndex and remove 
+    on time */
+    localStorage.setItem("todos", JSON.stringify(todos));
+/*     for saving updated todos into local storage */
+    }
+
